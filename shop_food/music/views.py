55bd9d9ad2b7b841c9560cbfc8b50bebd1efe7f2
Slogansudo <srmslogan6040@gmail.com>
2024-05-08 01:12:46 +0500

    change API music app add  filter_backends and pagination_class

--- shop_food/music/views.py
+++ shop_food/music/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from billing.models import Billing
@@ -8,6 +8,7 @@ from customers.models import Country, City, Address, Customers
 from .serializer import AddressSerializer, ProductSerializer, CustomersSerializer, CategorySerializer, CommentSerializer, CartSerializer, BillingSerializer,CountrySerializer, CitySerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import LimitOffsetPagination
 # Create your views here.
 
 
@@ -16,6 +17,12 @@ class CountryAPIViewSet(ModelViewSet):
     serializer_class = CountrySerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ['name', 'id']
+    pagination_class = LimitOffsetPagination
+
+    # http://api.example.org/accounts/?limit=100
+    # /?offset=400&limit=100
 
 
 class CityAPIViewSet(ModelViewSet):
@@ -23,6 +30,9 @@ class CityAPIViewSet(ModelViewSet):
     serializer_class = CitySerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ['name', 'country__name',]
+    pagination_class = LimitOffsetPagination
 
 
 class AddressAPIViewSet(ModelViewSet):
@@ -30,6 +40,9 @@ class AddressAPIViewSet(ModelViewSet):
     serializer_class = AddressSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ['name', 'city__name', 'city__country__name',]
+    pagination_class = LimitOffsetPagination
 
 
 class CustomersAPIViewSet(ModelViewSet):
@@ -37,6 +50,9 @@ class CustomersAPIViewSet(ModelViewSet):
     serializer_class = CustomersSerializer
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAdminUser, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ['first_name', 'last_name', 'username', 'email', 'address__name', 'address__city__name', 'address__city__country__name', 'phone_number']
+    pagination_class = LimitOffsetPagination
 
 
 class CommentAPIViewSet(ModelViewSet):
@@ -44,6 +60,9 @@ class CommentAPIViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['text', 'id', 'customer__first_name', 'customer__last_name', 'customer__username']
+    pagination_classes = LimitOffsetPagination
 
 
 class CategoryAPIViewSet(ModelViewSet):
@@ -51,6 +70,9 @@ class CategoryAPIViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['title', 'created_date']
+    pagination_class = LimitOffsetPagination
 
 
 class ProductAPIViewSet(ModelViewSet):
@@ -58,6 +80,9 @@ class ProductAPIViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['title', 'description', 'manufacturer_name', 'category__title', 'price', 'price_type', 'rating', 'max_weight', 'comments__text', 'comments__customer__username']
+    pagination_class = LimitOffsetPagination
 
 
 class CartAPIViewSet(ModelViewSet):
@@ -65,6 +90,9 @@ class CartAPIViewSet(ModelViewSet):
     serializer_class = CartSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['product__title', 'product__category__title', 'product__price', 'product__rating', 'product_number', 'product__comments__customer__username']
+    pagination_class = LimitOffsetPagination
 
 
 class BillingAPIViewSet(ModelViewSet):
@@ -72,3 +100,6 @@ class BillingAPIViewSet(ModelViewSet):
     serializer_class = BillingSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['customer__first_name', 'customer__last_name', 'customer__username', 'cart__total_price']
+    pagination_class = LimitOffsetPagination
