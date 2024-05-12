79b0923b9dfb316590985ffb2f3a44a1897c7b08
Slogansudo <srmslogan6040@gmail.com>
2024-05-12 22:40:49 +0500

    alter cart and billing vew and add action method

--- shop_food/core/settings.py
+++ shop_food/core/settings.py
@@ -153,15 +153,15 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-"""STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]"""
+
 
 STATIC_URL = 'static/'
-if DEBUG:
+"""if DEBUG:
     STATICFILES_DIRS = [BASE_DIR, 'static']
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')"""
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
